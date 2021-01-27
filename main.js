5a8f93aebb7d62bb72e6b040951862dbf8fe75f1
Sebastian Tomala <sebastian.tomalas@gmail.com>
2021-01-27 23:01:32 +0100

    change logic

--- main.js
+++ main.js
@@ -9,45 +9,68 @@ class Calculator {
         this.currentNum = this.currentNumEl.innerText;
     }
     clearAll() {
-        this.prevNum    = '';
-        this.currentNum = '';
-        this.result     = 0;
-        this.operation  = undefined;
+        this.prevNum       = '';
+        this.currentNum    = '';
+        this.result        = 0;
+        this.operation     = undefined;
         this.clrScreen();
     }
-    appendData(sign) {
-        this.currentNumEl.innerText = this.currentNumEl.innerText.toString() + sign.toString();
+    computeEq() {
+        this.compute(this.operation, this.prevNum, this.currentNum);
     }
-    setAction(action) {
-        if(action == '=') {
-            if(this.operation == undefined) return;
+    setOperation(action) {
+        if(    (this.prevNum == '') 
+            && (this.currentNum == '') 
+        ) {
+            return;
+        }
+        if(    (this.prevNum != '')
+            && (this.operation != undefined)
+            && (this.currentNum == '')
+        ) {
+            return;
+        }
+        if(    (this.prevNum != '')
+            && (this.operation != undefined)
+            && (this.currentNum != '')
+        ) {
             this.compute(this.operation, this.prevNum, this.currentNum);
-        }else {
-            if((this.prevNum == '')&&(this.currentNum == '')) {
-                this.operation = undefined;
-                return;
-            }else if((this.prevNum == '')&&(this.currentNum != '')) {
-                this.operation  = action;
-                this.prevNum    = this.currentNum;
-                this.prevNumEl.innerText = this.currentNumEl.innerText;
-                this.currentNumEl.innerText = '';
-                this.currentNum = '';
-                return;
-            }else {
-                this.prevNumEl.innerText = this.currentNumEl.innerText;
-                this.currentNumEl.innerText = '';
-                this.compute(this.operation, this.prevNum , this.currentNum);
-            }
+        }
+        if(    (this.prevNum != '')
+            && (this.operation == undefined)
+        ) {
+            this.currentNumEl.innerText = this.currentNumEl.innerText + action;
+            this.operation = action.toString();
         }
     }
-    setData(number) {
-        this.currentNum = this.currentNum.toString() + number.toString();
+    setNumber(number) {
+        if(    (this.prevNum == '') 
+            && (this.currentNum == '') 
+            ) {
+                this.prevNum = number.toString();
+                this.currentNumEl.innerText = this.prevNum;
+                return;
+            }
+        if(    (this.prevNum != '')
+            && (this.operation == undefined)
+            ) {
+                this.prevNum = this.prevNum.toString() + number.toString();
+                this.currentNumEl.innerText = this.prevNum;
+                return;
+            }
+        if(    (this.prevNum != '') 
+            && (this.operation != undefined)) {
+                this.currentNum             = this.currentNum .toString() + number.toString();
+                this.prevNumEl.innerText    = this.prevNum + this.operation;
+                this.currentNumEl.innerText = this.currentNum;
+                return;
+            }
     }
     compute(action, prev, current) {
-        let prevInt      = parseFloat(prev);
-        let currentInt   = parseFloat(current);
-        let operationFin = action;
-        let result       = 0;
+        let prevInt    = parseFloat(prev);
+        let currentInt = parseFloat(current);
+        let operation  = action;
+        let result     = 0;
         switch (action) {
             case '+':
                 this.result = currentInt + prevInt;
@@ -64,16 +87,21 @@ class Calculator {
             default:
                 break;
         }
-        this.prevNumEl.innerText = this.result;
+        this.prevNumEl.innerText    = this.result;
         this.currentNumEl.innerText = '';
-        this.currentNum = this.result;
-        this.prevNum = '';
+        this.currentNum = '';
+        this.operation  = undefined;
+        this.prevNum    = this.result;
+        this.result     = 0;
+
     }
     clrScreen() {
         this.currentNumEl.innerText = '';
         this.prevNumEl.innerText    = '';
     }
 }
+
+const eq           = document.querySelector('[data-eq]');
 const del          = document.querySelector('[data-del]');
 const point        = document.querySelector('[data-point]');
 const allClr       = document.querySelector('[data-ac]');
@@ -86,18 +114,16 @@ const calculator = new Calculator(currentNumEl, prevNumEl);
 
 numbers.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendData(button.textContent);
-        calculator.setData(button.textContent);
+        calculator.setNumber(button.textContent);
     })
 })
 action.forEach(singleAction => {
     singleAction.addEventListener('click', () => {
-        if(! (singleAction.textContent == '=')) calculator.appendData(singleAction.textContent);
-        calculator.setAction(singleAction.textContent);
+        calculator.setOperation(singleAction.textContent);
     })
 })
-del.addEventListener('click', () => {
-    calculator.delete();
+eq.addEventListener('click', () => {
+    calculator.computeEq();
 })
 allClr.addEventListener('click', () => {
     calculator.clearAll();
